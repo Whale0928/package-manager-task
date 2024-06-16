2dd70b425312605769525830478d9eb2576f881f
whale0928 <rlagusrl928@gmail.com>
2024-06-16 23:09:55 +0900

    chore: default data init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+-- Package 테이블에 초기 데이터 삽입
+INSERT INTO package (tracking_no)
+values ('111122223331'),
+       ('111122223332'),
+       ('987654321098'),
+       ('456789012345'),
+       ('654321098765');
+
+INSERT INTO package_images (package_id, filename, type)
+values (1, 'image1.jpg', 'jpg'),
+       (1, 'image2.jpg', 'jpg'),
+       (2, 'image3.jpg', 'jpg'),
+       (2, 'image4.jpg', 'jpg'),
+       (3, 'image5.jpg', 'jpg'),
+       (3, 'image6.jpg', 'jpg'),
+       (4, 'image7.jpg', 'jpg'),
+       (4, 'image8.jpg', 'jpg');
