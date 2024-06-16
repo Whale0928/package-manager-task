3f2eae29eb3d4be966a6583315bf8661ddb9d0db
whale0928 <rlagusrl928@gmail.com>
2024-06-16 22:57:43 +0900

    project setup

---
+++ src/main/java/app/task/PackageManagerTaskApplication.java
@@ -0,0 +1,13 @@
+package app.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageManagerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageManagerTaskApplication.class, args);
+    }
+
+}
